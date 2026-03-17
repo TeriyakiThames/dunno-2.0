24b1beae3773406eecb62459dbe73cc5eccd1cd9
TeriyakiThames <chiratt.thames@gmail.com>
2026-03-18 01:18:24 +0700

    Feat: Update route to go to Setup page

--- apps/web/src/app/[locale]/auth/callback/route.ts
+++ apps/web/src/app/[locale]/auth/callback/route.ts
@@ -3,26 +3,18 @@ import createClient from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin, pathname } = new URL(request.url);
-
-  // Extract auth code and optional redirect path
-  const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/";
-
-  // extract locale
   const locale = pathname.split("/")[1];
+  const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? `/${locale}/setup`;
 
   if (code) {
     const supabase = await createClient();
-
-    // Exchange the auth code for a session
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
-      // Redirect to the intended path or fallback to homepage
       return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
-  // Redirect to error page if code is missing or exchange fails
   return NextResponse.redirect(`${origin}/${locale}/auth/auth-code-error`);
 }
