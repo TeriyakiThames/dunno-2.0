36c29c33f97a33c5a23602760f3a7ba41bf481f3
TeriyakiThames <chiratt.thames@gmail.com>
2026-03-18 21:21:06 +0700

    Feat: Link arrow to home first
    - TODO: Edit when path is confirmed

--- apps/web/src/components/Setup/SetupTitle.tsx
+++ apps/web/src/components/Setup/SetupTitle.tsx
@@ -2,6 +2,7 @@ import { Locale, Messages } from "@calculories/shared-types";
 import LocaleSwitcher from "@/components/Shared/LocaleSwitcher";
 import Image from "next/image";
 import { t } from "@/lib/internationalisation/i18n-helpers";
+import Link from "next/link";
 
 export default function SetupTitle({
   locale,
@@ -13,10 +14,16 @@ export default function SetupTitle({
   return (
     <div className="mb-12.5 flex w-full items-center justify-between pt-7.5">
       {/* TODO: Add functionality after deciding where it should go back to */}
-      <button className="w-14.5">
-        <Image src="/Icons/Arrow.svg" alt="Arrow Icon" width={17} height={15} />
-      </button>
-
+      <Link href={`/${locale}`}>
+        <button className="w-14.5">
+          <Image
+            src="/Icons/Arrow.svg"
+            alt="Arrow Icon"
+            width={17}
+            height={15}
+          />
+        </button>
+      </Link>
       <h1 className="text-grey-100 text-2xl font-bold">
         {t("Quick Setup", messages)}
       </h1>
